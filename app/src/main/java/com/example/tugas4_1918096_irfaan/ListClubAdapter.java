3bd8de70325e0a06db6b6b7d7a6af68dacdf2e55
prayoga7 <muhammadirfaan25@gmail.com>
2021-10-27 23:23:26 +0700

    first commit

---
+++ app/src/main/java/com/example/tugas4_1918096_irfaan/ListClubAdapter.java
@@ -0,0 +1,76 @@
+package com.example.tugas4_1918096_irfaan;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListClubAdapter extends RecyclerView.Adapter<ListClubAdapter.ListViewHolder>{
+    private ArrayList<Club> listClub;
+
+    private OnItemClickCallback onItemClickCallback;
+
+    public void setOnItemClickCallback(OnItemClickCallback onItemClickCallback){
+         this.onItemClickCallback = onItemClickCallback;
+    }
+
+    public ListClubAdapter(ArrayList<Club>list){
+        this.listClub = list;
+    }
+    @NonNull
+    @Override
+    public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row_club, parent, false);
+        return new ListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
+        Club club = listClub.get(position);
+        Glide.with(holder.itemView.getContext())
+                .load(club.getPhoto())
+                .apply(new RequestOptions().override(55, 55))
+                .into(holder.imgPhoto);
+
+        holder.tvName.setText(club.getName());
+        holder.tvDetail.setText(club.getDetail());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 onItemClickCallback.onItemClicked(listClub.get(holder.getAbsoluteAdapterPosition()));
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return listClub.size();
+    }
+
+    public class ListViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgPhoto;
+        TextView tvName;
+        TextView tvDetail;
+
+        ListViewHolder(View itemview) {
+            super(itemview);
+            imgPhoto = itemview.findViewById(R.id.img_item_club);
+            tvName = itemview.findViewById(R.id.tv_item_name);
+            tvDetail = itemview.findViewById(R.id.tv_detail);
+        }
+    }
+    public interface OnItemClickCallback{
+        void onItemClicked(Club data);
+    }
+}
---
+++ app/src/main/java/com/example/tugas4_1918096_irfaan/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.tugas4_1918096_irfaan;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvClub;
+    private ArrayList<Club> list = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvClub = findViewById(R.id.rv_club);
+        rvClub.setHasFixedSize(true);
+
+        list.addAll(ClubData.getListData());
+        showRecyclerList();
+    }
+    private void showRecyclerList(){
+        rvClub.setLayoutManager(new LinearLayoutManager(this));
+        ListClubAdapter listClubAdapter = new ListClubAdapter(list);
+        rvClub.setAdapter(listClubAdapter);
+
+        listClubAdapter.setOnItemClickCallback(new ListClubAdapter.OnItemClickCallback() {
+            @Override
+            public void onItemClicked(Club data) {
+                showSelectedClub(data);
+            }
+        });
+
+    }
+    private void showSelectedClub(Club club){
+        Toast.makeText(this, "Kamu Memilih" + club.getName(),Toast.LENGTH_SHORT).show();
+    }
+}
